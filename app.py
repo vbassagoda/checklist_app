9bc057ad132630810daba7cfeb227b7fb5c62b45
Veronica Bassagoda <47826955+vbassa@users.noreply.github.com>
2020-02-22 23:56:15 -0300

    Error logic (#2)
    * add error logic
    
    * correct typo

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mac@localhost:5432/todoapp'
@@ -10,7 +11,6 @@ class Todo(db.Model):
   __tablename__ = 'todos'
   id = db.Column(db.Integer, primary_key=True)
   description = db.Column(db.String(), nullable=False)
-
   def __repr__(self):
     return f'<Todo {self.id} {self.description}>'
 
@@ -19,16 +19,27 @@ class Todo(db.Model):
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-  description = request.get_json()['description']  #fetches the JSON body sent, in this case fetches a dict with key 'description'
-  todo = Todo(description=description)
-  db.session.add(todo)
-  db.session.commit()
-  return jsonify({   #will return JSON data to the client
-    'description': todo.description
-  })
-  # return redirect(url_for('index')) #index is the name for the route handler that listens to changes on the index route
+  error = False
+  body = {}
+  try:
+    description = request.get_json()['description']  #fetches the JSON body sent, in this case fetches a dict with key 'description'
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    # todo instance we won't be available after closing sesion so we need to persist it:
+    body['description'] = todo.description
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info())
+  finally:
+    db.session.close() # return connections to the connection pool at the end of each session
+  if error:
+    abort(400)
+  else:
+    return jsonify(body)  # return JSON data to the client
 
 
 @app.route('/')
-def index():
+def index():  #index is the name for the route handler that listens to changes on the index route
   return render_template('index.html', data=Todo.query.all())
